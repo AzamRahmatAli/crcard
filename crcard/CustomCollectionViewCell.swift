f7bc537ea9d2e2eb033d327904120741bcc14d11
azamrahmatali <azam@brainloadtech.com>
2016-11-24 15:40:13 +0500

    first commit

---
+++ crcard/CustomCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.swift
+//  crcard
+//
+//  Created by Azam Rahmat on 11/23/16.
+//  Copyright © 2016 Brainload Technologies. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+}
