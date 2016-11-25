b2484774bbe7231ad6e23350ed0358ca2900f96f
azamrahmatali <azam@brainloadtech.com>
2016-11-25 17:33:23 +0500

    first commit

---
+++ CR Card Creater/CustomCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCollectionViewCell.swift
+//  crcard
+//
+//  Created by Azam Rahmat on 11/23/16.
+//  Copyright © 2016 Brainload Technologies. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var points: UILabel!
+    @IBOutlet weak var pointsd: UILabel!
+    /*override func awakeFromNib() {
+        imageView.layer.shadowColor = UIColor.blackColor().CGColor
+        imageView.layer.shadowOffset = CGSizeMake(-1, -1)
+        imageView.layer.shadowOpacity = 1
+        imageView.layer.shadowRadius = 1.0
+        imageView.clipsToBounds = false
+    }*/
+}
