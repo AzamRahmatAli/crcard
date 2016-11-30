f3af6b79af2252d2ff65244222436fa835b87c71
azamrahmatali <azam@brainloadtech.com>
2016-11-30 14:03:54 +0500

    create card controller

---
+++ CR Card Creater/CreateCardViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateCardViewController.swift
+//  CR Card Creater
+//
+//  Created by Azam Rahmat on 11/30/16.
+//  Copyright © 2016 Brainload Technologies. All rights reserved.
+//
+
+import UIKit
+
+class CreateCardViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
