1d05d4a8b3af13400a89f85256f414cfaf665538
azamrahmatali <azam@brainloadtech.com>
2016-12-01 15:39:40 +0500

    image pick working

--- CR Card Creater/CreateCardViewController.swift
+++ CR Card Creater/CreateCardViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CreateCardViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class CreateCardViewController: UIViewController, UITableViewDataSource, UITableViewDelegate,UIActionSheetDelegate , UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var tableHeight: NSLayoutConstraint!
 
     @IBOutlet weak var tableView: UITableView!
@@ -20,8 +20,14 @@ class CreateCardViewController: UIViewController, UITableViewDataSource, UITable
     @IBOutlet weak var rarity: UIImageView!
     @IBOutlet weak var type: UIImageView!
     @IBOutlet weak var elixirCost: UIImageView!
+    
+    
+    
+    var imagePicker: UIImagePickerController = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
        reloadTable() 
         // Do any additional setup after loading the view.
     }
@@ -34,6 +40,28 @@ class CreateCardViewController: UIViewController, UITableViewDataSource, UITable
     @IBAction func costEdit(sender: UIButton) {
     }
     @IBAction func addImage(sender: UIButton) {
+        //Create the AlertController and add Its action like button in Actionsheet
+        let actionSheetControllerIOS8: UIAlertController = UIAlertController(title: "", message: "Attach receipt image from", preferredStyle: .ActionSheet)
+        
+        
+        
+        let saveActionButton: UIAlertAction = UIAlertAction(title: "Camera", style: .Default)
+        { action -> Void in
+            self.getImageFromCamera()
+        }
+        actionSheetControllerIOS8.addAction(saveActionButton)
+        
+        let deleteActionButton: UIAlertAction = UIAlertAction(title: "Photo Library", style: .Default)
+        { action -> Void in
+            self.getImageFromGallery()
+        }
+        actionSheetControllerIOS8.addAction(deleteActionButton)
+        self.presentViewController(actionSheetControllerIOS8, animated: true, completion: nil)
+        
+        let cancelActionButton: UIAlertAction = UIAlertAction(title: "Cancel", style: .Cancel) { action -> Void in
+            
+        }
+        actionSheetControllerIOS8.addAction(cancelActionButton)
     }
     
     @IBAction func addAttribute(sender: UIButton) {
@@ -67,5 +95,38 @@ class CreateCardViewController: UIViewController, UITableViewDataSource, UITable
         tableHeight.constant = 2.5 * tableHeight.constant
         tableView.reloadData()
         //plus anything else you want to accomplish
+    }
+    
+    
+    
+    func getImageFromGallery() {
+        
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+     func performUIUpdatesOnMain(updates: () -> Void) {
+        dispatch_async(dispatch_get_main_queue()) {
+            updates()
+        }
+    }
+    
+    
+    func getImageFromCamera() {
+        
+        
+        
+        imagePicker.sourceType = .Camera
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+       /* performUIUpdatesOnMain
+            {*/
+        self.userImage.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        //}
     }
 }
